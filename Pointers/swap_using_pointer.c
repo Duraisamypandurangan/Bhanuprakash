23420d1c41c15e3625e6904bcaa0d9bc087e8fe3
“bhnup1997” <bhnup11@.com>
2021-07-02 16:55:31 +0530

    pointers

---
+++ Pointers/swap_using_pointer.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void swap(int *num1, int *num2);
+int main(void)
+{
+	int num1,num2;
+	printf("Enter the two numbers to swap :");
+	scanf("%d %d",&num1,&num2);
+	printf("Before swap num1= %d  num2= %d \n",num1,num2);
+	swap(&num1,&num2);
+	printf(" After swap num1= %d num2=%d \n",num1,num2);
+	return 0;
+}
+
+void swap(int *num1, int *num2)
+{
+	int temp=*num1;
+	*num1=*num2;
+	*num2=temp;
+}
+
