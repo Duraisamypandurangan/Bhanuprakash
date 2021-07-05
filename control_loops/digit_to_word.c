9622c5fdcc7ba242a7bca5523621016b52789f7b
“bhnup1997” <bhnup11@.com>
2021-07-05 16:54:57 +0530

    Updated digit to word code

--- control_loops/digit_to_word.c
+++ control_loops/digit_to_word.c
@@ -1,53 +1,50 @@
 #include<stdio.h>
+#include<string.h>
 int main(void)
 {
-	int num,rem,rev=0;
+	int count=0;
+	char Sstr[50];
 	printf("Enter the number :");
-	scanf("%d",&num);//read input
-	while(num > 0)//first find revers of number
+	fgets(Sstr,50,stdin);
+     do
 	{
-		rem=num%10;
-		rev=(rev*10)+rem;
-		num/=10;
-	}
-	while(rev > 0)
-	{
-		switch( rev % 10)//get last digit and print the word using case statement
+		switch(Sstr[count])//get last digit and print the word using case statement
 		{
-			case 0:
+			case '0':
 				printf("Zero ");
 				break;
-			case 1:
+			case'1':
 				printf("One ");
 				break;
-			case 2:
+			case'2':
 				printf("Two ");
 				break;
-			case 3:
+			case '3':
 				printf("Three ");
 				break;
-			case 4:
+			case '4':
 				printf("Four ");
 				break;
-			case 5:
+			case '5':
 				printf("Five ");
 				break;
-			case 6:
+			case '6':
 				printf("Six ");
 				break;
-			case 7:
+			case '7':
 				printf("Seven ");
 				break;
-			case 8:
+			case '8':
 				printf("Eight ");
 				break;
-			case 9:
+			case '9':
 				printf("Nine ");
 				break;
 		}
-		rev/=10; //to remove last digit in number
-	}
+	}while(Sstr[count++]!='\0');
+	
 	printf("\n");
+
 return 0;
 }
 
