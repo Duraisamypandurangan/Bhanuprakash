4323a403646fb346541c96e3c9b9426b5daaf7d2
“bhnup1997” <bhnup11@.com>
2021-07-09 16:15:27 +0530

    Preprocessors

---
+++ preprocessor/even_0dd.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include "smartcode.h"
+#include "smartcode.h"
+
+BEGIN
+
+int num;
+pf("Enter the num :");
+sf("%d",&num);
+if(num % 2)
+	pf("Odd number\n");
+	else
+	pf("Even number");
+
+	NEWLINE
+
+END
+
---
+++ preprocessor/smartcode.h
@@ -0,0 +1,9 @@
+#ifndef _SMARTCODE_H
+#define _SMARTCODE_H
+
+#define BEGIN int main(void){
+#define END }
+#define NEWLINE printf("\n");
+#define pf printf
+#define sf scanf
+#endif
