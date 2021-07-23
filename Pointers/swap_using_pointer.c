562a0f2bac5193aa1fc1c14904fa8907caf992e3
bhnup1997 <86414585+bhnup1997@users.noreply.github.com>
2021-07-23 14:16:11 +0530

    Update swap_using_pointer.c

--- Pointers/swap_using_pointer.c
+++ Pointers/swap_using_pointer.c
@@ -1,19 +1,19 @@
-#include<stdio.h>
-void swap(int *num1, int *num2);
-int main(void)
+#include<stdio.h> //Preprocessor Directive to Include stdio.h header file
+void swap(int *num1, int *num2);//Function Declaration
+int main(void) 
 {
-	int num1,num2;
+	int num1,num2;//Variable Declaration
 	printf("Enter the two numbers to swap :");
-	scanf("%d %d",&num1,&num2);
+	scanf("%d %d",&num1,&num2);//Read Input Values
 	printf("Before swap num1= %d  num2= %d \n",num1,num2);
-	swap(&num1,&num2);
+	swap(&num1,&num2);//Function Call to Swap 2 numbers
 	printf(" After swap num1= %d num2=%d \n",num1,num2);
-	return 0;
+	return 0;//for Successful Execution
 }
 
 void swap(int *num1, int *num2)
 {
-	int temp=*num1;
+	int temp=*num1;//Declaring Temp localy to Swap 2 numbers
 	*num1=*num2;
 	*num2=temp;
 }
